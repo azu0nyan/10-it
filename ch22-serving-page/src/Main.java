7340d7605cb5588283abbc325a072caad6512e6f
azu0nyan <nikgavgav@gmail.com>
2025-02-27 14:43:30 +0300

    spark-java

---
+++ ch22-serving-page/src/Main.java
@@ -0,0 +1,37 @@
+import  spark.Spark;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.ZonedDateTime;
+
+/*
+   com.sparkjava.spark-core:2.9.4
+
+ */
+public class Main {
+    static String page = "";
+    static {
+        try {
+            page = Files
+                    .readAllLines(Path.of("index.html"))
+                    .stream()
+                    .reduce((x, y) -> x + y)
+                    .get();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    static String getPage() {
+        var currentTime = ZonedDateTime.now();
+        var result = page.replace("{{content}}", currentTime.toString());
+        return result;
+    }
+    public static void main(String[] args) {
+
+
+        Spark.get("/", (request, response) -> getPage());
+        System.out.println("Serving on http://localhost:" + Spark.port());
+    }
+}
